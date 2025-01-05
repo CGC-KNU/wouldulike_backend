66a8ee1a4796e6052ff24fa311418e2f0a02afa7
JaeminRoh <djaeminr@gmail.com>
2025-01-05 17:36:54 +0900

    Django 버전 4.2.6으로 수정, runtime.txt 파일 추가하여 python 버전 명시, settings.py 파일에 정적 파일 수집되도록 수정 (25/1/5 17:36)

--- wouldulike_backend/settings.py
+++ wouldulike_backend/settings.py
@@ -166,7 +166,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # 정적 파일을 수집할 디렉토리
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
