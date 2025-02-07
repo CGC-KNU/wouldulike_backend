1b789b2d2c6ad0b411163e0a5cc0c5a9fa66ecec
JaeminRoh <djaeminr@gmail.com>
2025-02-07 21:34:24 +0900

    url에 함수 추가

--- food_by_type/urls.py
+++ food_by_type/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import get_random_foods
+from .views import get_unique_random_foods
 
 urlpatterns = [
     path('random-foods/', get_random_foods, name='get_random_foods'),
+    path('unique-random-foods/', get_unique_random_foods, name='get_unique_random_foods'),
 ]
